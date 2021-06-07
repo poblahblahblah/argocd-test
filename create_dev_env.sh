805771a886b20314c93ca87f4bfeabbecd380366
Patrick O’Brien <patrick.obrien@thetradedesk.com>
2021-06-07 16:41:38 -0700

    add production namespace

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -2,5 +2,6 @@
 
 kind create cluster --config kind_config.yaml
 kubectl create namespace argocd
+kubectl create namespace production
 kubectl -n argocd apply -f install.yaml
 kubectl -n argocd apply -f install-applicationset.yaml
